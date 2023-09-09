b80f1cb6adf0315a34418c442de731a42f5bb963
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2023-09-09 20:23:14 +0100

    feat: added images of starts on background

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="grid grid-cols-2 min-h-screen">
       {/* Left */}
-      <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden border-r border-white/10">
+      <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover">
         {/* Blur */}
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] bg-purple-700 -translate-y-1/2 translate-x-1/2 rounded-full blur-full" />
         {/* Stripes */}
@@ -10,7 +10,7 @@ export default function Home() {
       </div>
 
       {/* Right */}
-      <div className="flex flex-col p-16">
+      <div className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)] bg-cover">
         <div className="flex flex-1 items-center justify-center">
           <p className="text-center leading-relaxed w-[360px]">
             Você ainda não registrou nenhuma lembrança, comesse a{' '}
