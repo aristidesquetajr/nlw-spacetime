ea89612373b4f8688f54c3532cad7d3f52f9c05a
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2023-09-09 20:38:44 +0100

    feat: create link for sign in

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -5,8 +5,22 @@ export default function Home() {
       <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover">
         {/* Blur */}
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] bg-purple-700 -translate-y-1/2 translate-x-1/2 rounded-full blur-full" />
+
         {/* Stripes */}
         <div className="absolute right-2 top-0 bottom-0 w-2 bg-stripes" />
+
+        {/* Sign In */}
+        <a
+          href=""
+          className="flex items-center gap-3 text-left hover:text-gray-50 transition-colors"
+        >
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400"></div>
+
+          <p className="text-sm leading-snug max-w-[140px]">
+            <span className="underline">Crie sua conta</span> e salve suas
+            memórias!
+          </p>
+        </a>
       </div>
 
       {/* Right */}
