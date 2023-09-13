0cd909840aa58af4e3d62bcaa5da7b485ec9fae1
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2023-09-13 18:17:15 +0100

    feat: configure cors on fastify

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,13 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
+
 import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
+
+app.register(cors, {
+  origin: true,
+})
 
 app.register(memoriesRoutes)
 
