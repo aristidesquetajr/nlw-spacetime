eb3a092d945d913c2850a19afd1a8b3bd5a6f817
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2023-09-15 21:08:36 +0100

    feat(server): added jwt

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import 'dotenv/config'
 
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
@@ -10,6 +11,10 @@ const app = fastify()
 
 app.register(cors, {
   origin: true,
+})
+
+app.register(jwt, {
+  secret: 'Spacetime',
 })
 
 app.register(authRoutes)
